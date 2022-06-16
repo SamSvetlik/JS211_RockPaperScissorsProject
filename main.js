5e9b20ad7a745f24f8b5be088d3a471ee02c15b5
SamSvetlik <sam.svetlik@gmail.com>
2022-06-15 20:47:18 -0500

    unfinished

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ const rl = readline.createInterface({
 
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
-
+  if (hand1 === hand2) {
+    return "It's a tie!"
+  }
+  
   // Write code here
   // Use the unit test to see what is expected
 
